629cf6bdf400447bb97a83fcfe24dae72097490a
Felipe Yuri Inoue <inouefy@gmail.com>
2018-07-21 12:21:51 -0300

    Fix em Vendas/Alugueis Res Com

--- controllers/search.js
+++ controllers/search.js
@@ -65,6 +65,35 @@ module.exports = (app) => {
             else res.send(imovels)
         })
     })
+
+    app.get('/search/alugueis/:tipo&:bairro&:valor', (req, res, next) => {
+        var query = {}
+        query["isVenda"] = false
+        var reqTipo   = req.params.tipo
+        if(reqTipo !== 'null') query["tipo"] = reqTipo
+        var reqBairro = req.params.bairro
+        if(reqBairro !== 'null') query["bairro"] = reqBairro
+        var reqValor  = req.params.valor
+        if(reqValor !== 'null') query["valor"] = {$lt : reqValor}
+
+        Imovel.find(query, (err, imovels) => {
+            if(err) next(err)
+            else res.send(imovels)
+        })        
+    })
+    
+    app.get('/search/alugueis/all', (req, res, next) => {
+        var query = {}
+        query["isResidencial"] = true
+
+        Imovel.find(query, (err, imovels) => {
+            if(err) next(err)
+            else if(!imovels.length) res.send(null)
+            else res.send(imovels)
+            
+        })
+    })
+
     app.get('/search/alugueis/destaques', (req, res, next) => {
         var query = {}
         query["isDestaque"] = true
@@ -87,21 +116,5 @@ module.exports = (app) => {
             else if(!imovels.length) res.send(null)
             else res.send(imovels)
         })
-    })
-
-    app.get('/search/alugueis/:tipo&:bairro&:valor', (req, res, next) => {
-        var query = {}
-        query["isVenda"] = false
-        var reqTipo   = req.params.tipo
-        if(reqTipo !== 'null') query["tipo"] = reqTipo
-        var reqBairro = req.params.bairro
-        if(reqBairro !== 'null') query["bairro"] = reqBairro
-        var reqValor  = req.params.valor
-        if(reqValor !== 'null') query["valor"] = {$lt : reqValor}
-
-        Imovel.find(query, (err, imovels) => {
-            if(err) next(err)
-            else res.send(imovels)
-        })        
     })
 }
